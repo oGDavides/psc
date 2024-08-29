b10261ab1606c19ef3554d6654b2dbd8c01b5d85
Davi Gusmão Santos Silva <gusmaodavi79@gmail.com>
2024-08-29 14:40:02 +0000

    Aplicação que soma dois numeros inteiros

---
+++ olamundo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class OlaMundo
+public static void main (string[] args) {
+    Scanner teclado = new Scanner (system.in)
+    int num1, num2, result;
+    system.out.println("informe o primeiro numero ");
+    num1 + teclado.nextInt();
+    system.out.println("valor da variável num: "+ num);   
+    num2 = teclado.nextInt();      
+
+    result = num1 + num2;
+
+    system.out.println("Resultado: " + result);
+    system.exit(0);
+
+
